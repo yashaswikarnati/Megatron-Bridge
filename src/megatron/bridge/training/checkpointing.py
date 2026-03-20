3f1c012b995a4ee2235a526ecef34060c90aa086
Chen Cui <chcui@nvidia.com>
2026-03-19 17:01:01 -0700

    lint
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    Signed-off-by: Pavel Gein <pavel.gein@gmail.com>

--- src/megatron/bridge/training/checkpointing.py
+++ src/megatron/bridge/training/checkpointing.py
@@ -856,6 +856,7 @@ def save_checkpoint(
             comet_finalize_fn()
 
     if should_fire(callback_manager, "on_checkpoint_save"):
+
         def fire_callback():
             callback_manager.fire(
                 "on_checkpoint_save",
