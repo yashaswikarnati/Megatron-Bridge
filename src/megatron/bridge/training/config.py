6d57c0c8f3fcdd90416b46c8ddaaa09066f1a7ad
Maanu Grover <maanug@nvidia.com>
2026-03-24 17:25:21 -0600

    revert some property methods
    Signed-off-by: Maanu Grover <maanug@nvidia.com>

--- src/megatron/bridge/training/config.py
+++ src/megatron/bridge/training/config.py
@@ -140,14 +140,6 @@ class DistributedInitConfig(MTrainDistributedInitConfig):
     def lazy_init(self, value: bool) -> None:
         self.lazy_mpu_init = value
 
-    @property
-    def use_gloo_process_groups(self) -> bool:
-        return self.enable_gloo_process_groups
-
-    @use_gloo_process_groups.setter
-    def use_gloo_process_groups(self, value: bool) -> None:
-        self.enable_gloo_process_groups = value
-
 
 @dataclass(kw_only=True)
 class RerunStateMachineConfig(MTrainRerunStateMachineConfig):
@@ -606,22 +598,6 @@ class CheckpointConfig(MTrainCheckpointConfig):
 
     strict_fsdp_dtensor_load: bool = False
     """Whether to enforce strict loading for FSDP DTensor checkpoints. When False, allows partial loading."""
-
-    @property
-    def fully_parallel_save(self) -> bool:
-        return self.ckpt_fully_parallel_save
-
-    @fully_parallel_save.setter
-    def fully_parallel_save(self, value: bool) -> None:
-        self.ckpt_fully_parallel_save = value
-
-    @property
-    def fully_parallel_load(self) -> bool:
-        return self.ckpt_fully_parallel_load
-
-    @fully_parallel_load.setter
-    def fully_parallel_load(self, value: bool) -> None:
-        self.ckpt_fully_parallel_load = value
 
     def finalize(self) -> None:
         """Post-initialization checks for checkpoint config."""
